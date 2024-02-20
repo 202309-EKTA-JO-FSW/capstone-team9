1f829b55aca1709ae805a8312f30b05614d03914
fedabeqain <feda.beqain@gmail.com>
2024-02-20 20:21:08 +0300

    userAPI

--- server/server/controllers/User.js
+++ server/server/controllers/User.js
@@ -1,5 +1,22 @@
 const User = require('../models/User')
 
+//Get All user
+
+const getAllUser = async(req,res)=>{
+
+  try
+  {
+const GetAlluser = await User.find();
+res.json(GetAlluser)
+
+  }
+  catch(error)
+  {
+   return res.status(401).send({error : 'There is No User! '})
+  }
+}
+//Get a single user
+
 const getuserDetials = async(req,res)=>{
     const {id} = req.params;
 
@@ -14,5 +31,43 @@ const getuserDetials = async(req,res)=>{
     }
 }
 
+// Update a user
 
-module.exports = { getuserDetials}
+const updatedUser = async (req, res) => {
+  const { _id } = req.user;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      _id,
+      {
+        FullName: req?.body?.FullName,
+        UserName: req?.body?.UserName,
+        Email: req?.body?.Email,
+        PhoneNumber: req?.body?.PhoneNumber,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json(updatedUser);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+// delete a single user
+
+const deleteaUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deleteaUser = await User.findByIdAndDelete(id);
+    res.json({
+      deleteaUser,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+module.exports = { getuserDetials,updatedUser,deleteaUser,getAllUser}
--- server/server/routes/UserRoute.js
+++ server/server/routes/UserRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const User = require('../controllers/user');
-
-
+const isAdmin = require("../middleware/authmiddleware")
+router.get("/all-user",User.getAllUser);
 router.get("/:id",User.getuserDetials);
+router.put("/:id",isAdmin, User.updatedUser);
+router.delete("/:id",isAdmin,User.deleteaUser);
 
 
 
